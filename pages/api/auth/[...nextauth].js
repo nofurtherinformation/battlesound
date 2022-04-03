2fc8846f5f307163638fc6dbf7868d6680c2acd3
Dylan <dhalpern@uchicago.edu>
2022-04-03 15:54:28 -0500

    debug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth"
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-
 const prisma = new PrismaClient()
 
 export default NextAuth({
@@ -20,5 +19,6 @@ export default NextAuth({
       from: process.env.EMAIL_FROM
     })
   ],
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+  debug: true
 })
