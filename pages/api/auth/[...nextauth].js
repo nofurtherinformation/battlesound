6950a8b69759ceb5c1f45f56083595159bab6784
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-04-05 09:23:53 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,60 +1,34 @@
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
-// import { PrismaAdapter } from '@next-auth/prisma-adapter'
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient()
-import { FaunaAdapter } from "@next-auth/fauna-adapter"
-import { Client as FaunaClient } from "faunadb"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-const client = new FaunaClient({
-	secret: process.env.FDB_SECRET
-})
-
-export default function auth(...args) {
-	console.log('STARTING AUTH')
-	// console.log(prisma)
-	return NextAuth({
-		adapter: FaunaAdapter(client),
-		providers: [
-			EmailProvider({
-				server: {
-					host: process.env.EMAIL_SERVER_HOST,
-					port: process.env.EMAIL_SERVER_PORT,
-					auth: {
-						user: process.env.EMAIL_SERVER_USER,
-						pass: process.env.EMAIL_SERVER_PASSWORD
-					}
-				},
-				from: process.env.EMAIL_FROM
-			})
-		],
-		session: {
-			jwt: true,
-			maxAge: 7 * 24 * 60 * 60
-		},
-		jwt: {
-			secret: process.env.SECRET,
-			encryption: true
-		},
-		secret: process.env.SECRET,
-		debug: true,
-		callbacks: {
-			async signIn({ user, account, profile, email, credentials }) {
-				console.log('SIGNED IN')
-				return true
+const AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		EmailProvider({
+			server: {
+				host: process.env.EMAIL_SERVER_HOST,
+				port: process.env.EMAIL_SERVER_PORT,
+				auth: {
+					user: process.env.EMAIL_SERVER_USER,
+					pass: process.env.EMAIL_SERVER_PASSWORD
+				}
 			},
-			async redirect({ url, baseUrl }) {
-				console.log('REDIRECTED')
-				return baseUrl
-			},
-			async session({ session, user, token }) {
-				console.log('SESSION CREATED')
-				return session
-			},
-			async jwt({ token, user, account, profile, isNewUser }) {
-				console.log('TOKEN GENERATED')
-				return token
-			}
-		}
-	})(...args)
+			from: process.env.EMAIL_FROM
+		})
+	],
+	session: {
+		jwt: true,
+		maxAge: 7 * 24 * 60 * 60
+	},
+	jwt: {
+		secret: process.env.JWT_SECRET,
+		encryption: true
+	},
+	secret: process.env.SECRET,
+	debug: true
 }
+
+export default NextAuth(AuthOptions)
