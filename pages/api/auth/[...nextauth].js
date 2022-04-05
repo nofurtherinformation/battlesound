229f5bd756057c419520396efe848df18746a801
Dylan <dhalpern@uchicago.edu>
2022-04-04 20:38:02 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,29 +5,51 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default NextAuth({
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		EmailProvider({
-			server: {
-				host: process.env.EMAIL_SERVER_HOST,
-				port: process.env.EMAIL_SERVER_PORT,
-				auth: {
-					user: process.env.EMAIL_SERVER_USER,
-					pass: process.env.EMAIL_SERVER_PASSWORD
-				}
-			},
-			from: process.env.EMAIL_FROM
-		})
-	],
-	session: {
-		jwt: true,
-		maxAge: 7 * 24 * 60 * 60
-	},
-	jwt: {
+export default function auth(...args) {
+	console.log('STARTING TO AUTH', ...args)
+	console.log(prisma)
+	return NextAuth({
+		adapter: PrismaAdapter(prisma),
+		providers: [
+			EmailProvider({
+				server: {
+					host: process.env.EMAIL_SERVER_HOST,
+					port: process.env.EMAIL_SERVER_PORT,
+					auth: {
+						user: process.env.EMAIL_SERVER_USER,
+						pass: process.env.EMAIL_SERVER_PASSWORD
+					}
+				},
+				from: process.env.EMAIL_FROM
+			})
+		],
+		session: {
+			jwt: true,
+			maxAge: 7 * 24 * 60 * 60
+		},
+		jwt: {
+			secret: process.env.SECRET,
+			encryption: true
+		},
 		secret: process.env.SECRET,
-		encryption: true
-	},
-	secret: process.env.SECRET,
-	debug: true
-})
+		debug: true,
+		callbacks: {
+			async signIn({ user, account, profile, email, credentials }) {
+				console.log('SIGNED IN')
+				return true
+			},
+			async redirect({ url, baseUrl }) {
+				console.log('REDIRECTED')
+				return baseUrl
+			},
+			async session({ session, user, token }) {
+				console.log('SESSION CREATED')
+				return session
+			},
+			async jwt({ token, user, account, profile, isNewUser }) {
+				console.log('TOKEN GENERATED')
+				return token
+			}
+		}
+	})(...args)
+}
